badef023e4a529f69818643e86281a13c5bbae3f
Abhishek Singh <abhishek.a.singh@workday.com>
2024-11-06 18:34:40 -0700

    Setting and getting data from edge data server.

--- zxnia/src/client.rs
+++ zxnia/src/client.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, client!");
+// use tonic::{transport::Server, Request, Response, Status};
+use simpledb::{
+    edge_db_client::{EdgeDbClient},
+    Data,
+};
+
+pub mod simpledb {
+    tonic::include_proto!("simpledb");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = EdgeDbClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(Data {
+        key: "Zxnia".into(),
+        value: "".into(),
+    });
+
+    println!("Sending set_data request to gRPC Server...");
+    let response = client.set_data(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    println!("Sending get_data request to gRPC Server...");
+
+    let request = tonic::Request::new(Data {
+        key: "Zxnia".into(),
+        value: "".into(),
+    });
+    let response = client.get_data(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
--- zxnia/src/edge.rs
+++ zxnia/src/edge.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, edge!");
+use tonic::{transport::Server, Request, Response, Status};
+use simpledb::{
+    edge_db_server::{EdgeDb, EdgeDbServer},
+    Data, Dummy,
+};
+
+pub mod simpledb {
+    tonic::include_proto!("simpledb");
+}
+
+#[derive(Debug, Default)]
+pub struct Edge {}
+
+#[tonic::async_trait]
+impl EdgeDb for Edge {
+    async fn set_data(
+        &self,
+        request: Request<Data>,
+    ) -> Result<Response<Dummy>, Status> {
+        println!("Received request from: {:?}", request);
+        let response = simpledb::Dummy{};
+
+        Ok(Response::new(response))
+    }
+
+    async fn get_data(
+        &self,
+        request: Request<Data>
+    ) -> Result<Response<Data>, Status> {
+        println!("Received request from: {:?}", request);
+        let response = simpledb::Data {
+            key: format!("{}", request.into_inner().key).into(),
+            value: format!("Hello {}!", "Test Data Value").into(),
+        };
+        Ok(Response::new(response))
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let edge_service = Edge::default();
+
+    println!("Starting gRPC Server...");
+    Server::builder()
+        .add_service(EdgeDbServer::new(edge_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
