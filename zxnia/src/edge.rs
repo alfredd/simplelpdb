df97135d5f7e00fbca9e59dafebdd4a042395d81
Abhishek Singh <abhishek.a.singh@workday.com>
2024-11-07 17:09:40 -0700

    Skeleton for Edge Replica.

--- zxnia/src/edge.rs
+++ zxnia/src/edge.rs
@@ -1,7 +1,9 @@
 use tonic::{transport::Server, Request, Response, Status};
 use simpledb::{
     edge_db_server::{EdgeDb, EdgeDbServer},
-    Data, Dummy,
+    Data, Dummy, DataBlock,Certificate,
+    edge_replica_server::{EdgeReplica},
+    edge_certification_server::{EdgeCertification}
 };
 
 pub mod simpledb {
@@ -32,6 +34,30 @@ impl EdgeDb for Edge {
             key: format!("{}", request.into_inner().key).into(),
             value: format!("Hello {}!", "Test Data Value").into(),
         };
+        Ok(Response::new(response))
+    }
+}
+
+#[tonic::async_trait]
+impl EdgeReplica for Edge {
+    async fn replicate(
+        &self,
+        request: Request<DataBlock>
+    ) -> Result<Response<Dummy>, Status> {
+        println!("Received request from: {:?}", request);
+        let response = simpledb::Dummy {};
+        Ok(Response::new(response))
+    }
+}
+
+#[tonic::async_trait]
+impl EdgeCertification for Edge {
+    async fn certify(
+        &self,
+        request: Request<Certificate>
+    ) -> Result<Response<Dummy>, Status> {
+        println!("Received Certification request from: {:?}", request);
+        let response = simpledb::Dummy {};
         Ok(Response::new(response))
     }
 }
